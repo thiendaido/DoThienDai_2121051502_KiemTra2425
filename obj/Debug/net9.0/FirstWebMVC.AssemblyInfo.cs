b812c7582f76984a2415167e881f246732d57460
thiendaido <thiendaido2309@gmail.com>
2025-05-23 22:42:14 +0700

    đẩy file làm bài kiểm tra

--- obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293f614d0ef56f51a66351d95647f1ec52b3c49e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d7927c92fb944cdc114890cc577ff938a5204c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
